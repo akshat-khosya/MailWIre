312fc40458858366f6ee506bc7c323949c223dc8
akshat-khosya <akshatdps12@gmail.com>
2022-01-26 14:02:30 +0530

    _redirects

--- src/App.js
+++ src/App.js
@@ -6,18 +6,18 @@ import Login from "./pages/Login";
 import IndexPage from "./components/Index";
 function App() {
   const location=useLocation();
-  console.log(location.pathname);
+  console.log(location.pathname.split("/")[1]);
   return (
    
       <div className="h-fit min-h-screen w-screen flex flex-col  justify-between bg-gray-800">
-        {location.pathname==='/dashboard' ?<IndexPage />:""}
-       {location.pathname==='/dashboard'?"":<Navbar />}
+        {location.pathname.split("/")[1]==='dashboard' ?<IndexPage />:""}
+       {location.pathname.split("/")[1]==='dashboard'?"":<Navbar />}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/signin" element={<Login />} />
           
         </Routes>
-        {location.pathname==='/dashboard'?"":<Footer />}
+        {location.pathname.split("/")[1]==='dashboard'?"":<Footer />}
       </div>
   
   );
--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -276,10 +276,11 @@ export default function IndexPage() {
                         {/* Remove class [ h-64 ] when adding a card block */}
                         <div className="container mx-auto py-10 h-64 md:w-4/5 w-11/12 px-6">
                             {/* Remove class [ border-dashed border-2 border-gray-300 ] to remove dotted border */}
-                            <div className="w-full h-full rounded border-dashed border-2 border-gray-300">{/* Place your content here */}
+                            <div className="w-full h-full rounded ">{/* Place your content here */}
                             
                             <Routes>
                             <Route path="/dashboard" element={<Login />} />
+                            <Route path="/dashboard/product" element={<Login />} />
                             </Routes>
                             
                             </div>
